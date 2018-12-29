ec38194ca9a759ade870d018f03c7c8038f42bbe
Robert Snyder <r.snyder49@gmail.com>
2018-12-29 00:10:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ function getToken() {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  const url   = `${baseURL}/repos/${repo}/forks`;
+  fetch(url, {
+   method: 'POST',
+   headers: {
+     Authorization: `token ${getToken()}`
+   }
+ })
+ .then(res => res.json())
+ .then(json => showResults(json));
 }
 
 function showResults(json) {
